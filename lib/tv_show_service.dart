26630d28ae6c7b708e5dfab009664c4227280cc8
Guilherme Santos <g.santos@outlook.com>
2025-07-29 20:35:11 -0300

    Aula 29/07 - Desafio, TvShowScreen, FavTvShowScreen

--- lib/tv_show_service.dart
+++ lib/tv_show_service.dart
@@ -4,6 +4,19 @@ import 'package:app3_series_api/tv_show_model.dart';
 import 'package:http/http.dart' as http;
 
 class TvShowService {
+
+  Future<TvShow> fetchTvShowById(int id) async {
+    final response = await http.get(
+      Uri.parse('https://api.tvmaze.com/shows/$id'),
+    );
+
+    if (response.statusCode == 200) {
+      return TvShow.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Falha ao carregar série!');
+    }
+  }
+
   Future<List<TvShow>> fetchTvShows(String query) async {
     final response = await http.get(
       Uri.parse('https://api.tvmaze.com/search/shows?q=$query'),
@@ -19,4 +32,5 @@ class TvShowService {
       throw Exception('Falha ao carregar séries!');
     }
   }
+
 }
